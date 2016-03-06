f21b1d194970b0fa5f893dc0e2ba61a76ad8bf37
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2016-03-06 13:42:43 +0100

    code-style

--- app/code/community/Ffuenf/OrderExporter/Block/Adminhtml/Exporter/Edit/Tabs.php
+++ app/code/community/Ffuenf/OrderExporter/Block/Adminhtml/Exporter/Edit/Tabs.php
@@ -18,27 +18,27 @@
 
 class Ffuenf_OrderExporter_Block_Adminhtml_Exporter_Edit_Tabs extends Mage_Adminhtml_Block_Widget_Tabs
 {
-  public function __construct()
-  {
-      parent::__construct();
-      $this->setId('exporter_tabs');
-      $this->setDestElementId('edit_form');
-      $this->setTitle(Mage::helper('ffuenf_orderexporter')->__('Import Orders'));
-  }
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setId('exporter_tabs');
+        $this->setDestElementId('edit_form');
+        $this->setTitle(Mage::helper('ffuenf_orderexporter')->__('Import Orders'));
+    }
 
-  /**
-   * @return Mage_Adminhtml_Block_Widget_Tabs
-   */
-  protected function _beforeToHtml()
-  {
-      $this->addTab(
-          'form_section',
-          array(
+    /**
+     * @return Mage_Adminhtml_Block_Widget_Tabs
+     */
+    protected function _beforeToHtml()
+    {
+        $this->addTab(
+        'form_section',
+        array(
             'label'   => Mage::helper('ffuenf_orderexporter')->__('Import Orders'),
             'title'   => Mage::helper('ffuenf_orderexporter')->__('Import Orders'),
             'content' => $this->getLayout()->createBlock('exporter/adminhtml_exporter_edit_tab_form')->toHtml()
-          )
-      );
-      return parent::_beforeToHtml();
-  }
+                )
+        );
+        return parent::_beforeToHtml();
+    }
 }
